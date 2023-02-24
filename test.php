aafa7857e3964bdc3748f31e192ac71a4165c6b2
Jaturaphum <suaprakhon2002@gmail.com>
2023-02-25 00:39:06 +0700

    first commit

--- test.php
+++ test.php
@@ -1,129 +1,18 @@
 <?php
+require("connect_db.php");
 session_start();
-include('server.php');
 
-if (!isset($_SESSION['username'])) {
-    $_SESSION['msg'] = "You must log in first";
-    header('location: login.php');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = $_SESSION['username'];
+  $sql = "SELECT account FROM username ='username';";
+  $result = $conn->query($sql);
+  if (isset($_POST["deposit"])) {
+      $amount = $_POST["amount"];
+      $query = "UPDATE account SET deposit = deposit, balance = balance + '$amount' WHERE username = '$username'";
+      mysqli_query($conn, $query);
+  } else if (isset($_POST["withdraw"])) {
+      $amount = $_POST["amount"];
+      $query = "UPDATE account SET withdraw = withfraw ,balance = balance - '$amount' WHERE username = '$username'";
+      mysqli_query($conn, $query);
+  }
 }
-
-if (isset($_GET['logout'])) {
-    session_destroy();
-    unset($_SESSION['username']);
-    header('location: login.php');
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="style_index.css">
-</head>
-
-<body>
-    <div>
-        <form method="post">
-            <div class="section_area_grid">
-            <body onLoad ="initClock()">
-      <div id="timedate">
-      <a id="h">12</a>:
-        <a id="m">00</a>:
-        <a id="s">00</a><br>
-        <a id="mon">January</a>
-        <a id="d">1</a>
-        <a id="y">0</a>
-      </div>
-    </body>
-                <h2>Home</h2>
-                <div class="dropdown" style="float: right;">
-                    <p id="dropbtn">username: <strong style="margin-right: 5px;"><?php echo $_SESSION['username']; ?></strong>
-                    <p id="dropbtn">wallet: <strong style="margin-right: 5px;">
-                            <?php
-                            $errors = array();
-                            if (!$conn) {
-                                die("Connection failed: " . mysqli_connect_error());
-                            }
-
-                            $sql = "SELECT * FROM users ";
-                            $result = mysqli_query($conn, $sql);
-                            if (mysqli_num_rows($result) > 0) {
-
-                                while ($row = mysqli_fetch_assoc($result)) {
-                                    if ($_SESSION['username'] == $row["username"]) {
-                                        echo  $row["money"];
-                                    }
-                                }
-                            }
-                            mysqli_close($conn);
-                            ?>
-                        </strong> baht</p>
-                    </p>
-                    <div class="dropdown-content">
-                    </div>
-                </div>
-            </div>
-            <nav class="nav">
-                <ul>
-                    <li><a href="index.php">Home</a>
-                    <li><a href="deposit.php">deposit</a>
-                    <li><a href="withdraw.php">withdraw</a>
-                    <li><a href="listbank.php">list</a>
-                    <li><a href="index.php?logout='1'" style="color: red; text-decoration:none;">Logout</a>
-                </ul>
-            </nav>
-            <div class="img">
-                <center>
-                    <img src="img/money.jpg">
-                </center>
-            </div>
-            <footer>
-                <p class="main">
-                    2023 © jaturaphum.s@ku.th | jaturaphum Suaprakhon | develop by <a href="https://github.com/Jaturaphum" target="_blank">Jaturaphum</a>
-                </p>
-            </footer>
-        </form>
-    </div>
-</body>
-<script src="js/index.js"></script>
-<script>
-    Number.prototype.pad = function (n) {
-  for (var r = this.toString(); r.length < n; r = 0 + r);
-  return r;
-};
-function updateClock() {
-  var now = new Date();
-  var sec = now.getSeconds(),
-    min = now.getMinutes(),
-    hou = now.getHours(),
-    mo = now.getMonth(),
-    dy = now.getDate(),
-    yr = now.getFullYear();
-  var months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  var tags = ["mon", "d", "y", "h", "m", "s",],
-    corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2),];
-  for (var i = 0; i < tags.length; i++)
-    document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
-}
-
-function initClock() {
-  updateClock();
-  window.setInterval("updateClock()", 1);
-}
-</script>
-</html>
